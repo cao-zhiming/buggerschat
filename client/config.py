525c55eda4351668b181bc11513f4d12590e5024
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2022-08-20 21:53:32 +0800

    Add the address config

--- client/config.py
+++ client/config.py
@@ -1,3 +1,3 @@
-SERVER_IP = "127.0.0.1"
+ADDRESS = ("127.0.0.1",65535)  # 请使用你的服务器的公网地址和端口，端口务必与server设置一致 / Please use the PUBLIC ADDRESS of your server, and the port given here should be the very same with server
 
 LANG = "zh-CN"
