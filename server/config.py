bdfc503d9af4f705dc2f1b46f739e4617f2e22ab
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2022-08-20 21:50:23 +0800

    Add the config for the address of the server

--- server/config.py
+++ server/config.py
@@ -1,1 +1,3 @@
+ADDR = ("127.0.0.1",65535)  # 在这里请使用你的服务器的内网地址，端口应为1-65535的正整数 / Please use the PRIVATE ADDRESS of your server right here, port number should be a positive integer between 1 and 65535
+
 LANG = "zh-CN"
